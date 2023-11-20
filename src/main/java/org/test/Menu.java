abd36131d1e9428a82ab3cf6035500f279c26ec6
McSymilian <max.ryder8@gmail.com>
2023-11-20 22:06:23 +0100

    Restyle cleanups

--- src/main/java/org/test/Menu.java
+++ src/main/java/org/test/Menu.java
@@ -1,9 +1,11 @@
 package org.test;
 
-import org.test.animals.*;
+import org.test.animals.Animal;
+import org.test.animals.Cow;
+import org.test.animals.Livestock;
+import org.test.animals.Pig;
+import org.test.animals.Sheep;
 
-import java.sql.SQLOutput;
-import java.util.Objects;
 import java.util.Scanner;
 
 import static org.test.Main.animals;
@@ -20,9 +22,11 @@ public class Menu {
 
     }
     private static final Scanner sc = new Scanner(System.in);
+
     public void setNextAction() {
         nextAction = sc.nextInt();
     }
+
     private static final MenuAction showAllAction =()-> {
         for (Animal animal : animals) {
             System.out.println(animal);}
@@ -30,6 +34,7 @@ public class Menu {
     public MenuAction getMenuAction() {
         return switch (nextAction) {
             case 1 -> showAllAction;
+
             case 2-> () -> {
                 for (Animal animal: animals) {
                     if (animal instanceof Sheep sheep) {
@@ -37,59 +42,74 @@ public class Menu {
                     }
                 }
             };
-            case 3-> () -> {
+
+            case 3 -> () -> {
                 for (Animal animal: animals) {
                     if (animal instanceof Cow cow) {
                         System.out.println(cow);
                     }
                 }
             };
-            case 4-> () -> {
+
+            case 4 -> () -> {
                 for (Animal animal: animals) {
                     if (animal instanceof Pig pig) {
                         System.out.println(pig);
                     }
                 }
             };
-            case 5-> () -> {
+
+            case 5 -> () -> {
                 System.out.println("select animal to add: Cow (C) or Sheep (S) or Pig (P)");
                 char aniamlChose = sc.next().charAt(0);
+
                 System.out.println("Set VetID");
                 int vetID = sc.nextInt();
+
                 System.out.println("Set ID");
                 int animalID = sc.nextInt();
+
                 switch (aniamlChose){
                     case 'C':
                         animals.add(new Cow(String.valueOf(vetID), String.valueOf(animalID)));
                         break;
+
                     case 'S':
                         animals.add(new Sheep(String.valueOf(vetID), String.valueOf(animalID)));
                         break;
+
                     case 'P':
                         System.out.println("set mass of Pig");
                         animals.add(new Pig(String.valueOf(vetID), String.valueOf(animalID), sc.nextDouble()));
                 }
             };
+
             case 6->()-> {
                 showAllAction.doAction();
+
                 System.out.println("chose \"vetID\" -> 1, \"ID\" -> 2, \"index\" -> 3.");
                 int userChose = sc.nextInt();
+
                 switch (userChose){
                     case 1:
                         Livestock temporary;
+
                         do {
                             System.out.println("write vetID");
                             int userVetID = sc.nextInt();
                             temporary = animals.stream()
                                     .map((a) -> (Livestock) a)
-                                    .filter(a -> "a".equals(a.getVetID()))
+                                    .filter(a -> String.valueOf(userVetID).equals(a.getVetID()))
                                     .findFirst()
                                     .orElse(null);
                         } while  (temporary != null);
+
                         animals.remove(temporary);
+                        break;
+                    //TODO case 2 usuniecie  ID, Usunięcie indexu
                 }
             };
-            //TODO case 2 usuniecie  ID, Usunięcie indexu
+
             default -> () -> {};
         };
     }
